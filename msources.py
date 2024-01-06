d77e119295b840952b5bf5aaca5aa795b7e65af3
Coufal, Jan <jan.coufal@oracle.com>
2024-01-06 16:21:33 +0100

    Source -> ScrapSource

--- msources.py
+++ msources.py
@@ -1,14 +1,14 @@
 from enum import Enum
 
 
-class Source(Enum):
+class ScrapSource(Enum):
 	NOOP = "noop"
 	ROUMEN = "roumen"
 	ROUMEN_MASO = "roumen-maso"
 
 	@staticmethod
 	def of(source):
-		for e in Source:
+		for e in ScrapSource:
 			if e.value == source:
 				return e
-		return Source.NOOP
+		return ScrapSource.NOOP
