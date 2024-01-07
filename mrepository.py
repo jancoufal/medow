95eec1d35d667adca154723c3d1b91d597775a34
Coufal, Jan <jan.coufal@oracle.com>
2024-01-07 17:54:04 +0100

    repository entities

--- mrepository.py
+++ mrepository.py
@@ -2,14 +2,12 @@
 persistent repository implementation
 """
 
-from abc import ABCMeta, abstractmethod
-from dataclasses import dataclass
+from abc import ABCMeta
 from typing import List
 from enum import Enum
-from datetime import datetime
 from msqlite_api import SqliteApi
 from mscrapsources import ScrapSource
-from mformatters import Formatter, TimestampFormat
+from mrepositoryentities import *
 
 
 class _Tables(Enum):
@@ -28,92 +26,15 @@ class RepositoryInterface(ABCMeta):
 	pass
 
 
-@dataclass
-class SourceScrapperItem:
-	source: str
-	scrap_timestamp: datetime
-	name: str
-	local_path: str
-	impressions: int
-
-	@property
-	def age(self) -> str:
-		return Formatter.ts_diff_to_str(self.scrap_timestamp, datetime.now(), False)
-
-	@property
-	def scrap_timestamp_str(self):
-		return Formatter.ts_to_str(TimestampFormat.DATETIME)
-
-
-@dataclass
-class SourceScrapperScrap:
-	scrap_id: int
-	source: str
-	status: str
-	scrap_start: datetime | None
-	scrap_end: datetime | None
-	count_success: int
-	count_fail: int
-	exc_type: str | None
-	exc_value: str | None
-	exc_traceback: str | None
-
-	@property
-	def ts_start(self) -> str:
-		return Formatter.NOT_AVAILABLE_STR if self.scrap_start is None else Formatter.ts_to_str(TimestampFormat.DATETIME, self.scrap_start)
-
-	@property
-	def ts_end(self) -> str:
-		return Formatter.NOT_AVAILABLE_STR if self.scrap_end is None else Formatter.ts_to_str(TimestampFormat.DATETIME, self.scrap_end)
-
-	@property
-	def age(self) -> str:
-		return Formatter.NOT_AVAILABLE_STR if self.scrap_start is None else Formatter.ts_diff_to_str(self.scrap_start, datetime.now(),False)
-
-	@property
-	def time_taken(self) -> str:
-		return Formatter.NOT_AVAILABLE_STR if None in (self.scrap_start, self.scrap_end) else Formatter.ts_diff_to_str(self.scrap_start, self.scrap_end, False)
-
-	@property
-	def success_percentage(self) -> str:
-		return Formatter.percentage_str_safe(self.count_success, self.count_success + self.count_fail, Formatter.NOT_AVAILABLE_STR)
-
-
 class RepositorySourceScrapper(object):
 	def __init__(self, sqllite_api: SqliteApi, scrap_source: ScrapSource):
 		super().__init__()
 		self._sqllite_api = sqllite_api
 		self._scrap_source = scrap_source
 
-	def read_recent_items(self, item_limit: int) -> List[SourceScrapperItem]:
-		def _row_mapper(r) -> SourceScrapperItem:
-			return SourceScrapperItem(
-				source=self._scrap_source.value,
-				scrap_timestamp=Formatter.str_to_ts(TimestampFormat.DATETIME_MS, f"{r[0]} {r[1]}"),
-				name=r[2],
-				local_path=r[3],
-				impressions=r[4]
-			)
-
-		stmt = f"""
-			select ts_date, ts_time, name, local_path, impressions
-			from {_Tables.SCRAP_ITEMS.value}
-			inner join {_Tables.SCRAP_STAT.value}
-				on {_Tables.SCRAP_STAT.value}.scrap_stat_id={_Tables.SCRAP_ITEMS.value}.scrap_stat_id
-			where source=:source
-			order by ts_date desc, ts_time desc
-			limit :limit"""
-
-		binds = {
-			"source": self._scrap_source.value,
-			"limit": item_limit
-		}
-
-		return self._sqllite_api.read(stmt, binds, _row_mapper)
-
-	def read_recent_scraps(self, item_limit: int) -> List[SourceScrapperScrap]:
-		def _row_mapper(r) -> SourceScrapperScrap:
-			return SourceScrapperScrap(
+	def read_recent_scraps(self, item_limit: int) -> List[ScrapStatEntity]:
+		def _row_mapper(r) -> ScrapStatEntity:
+			return ScrapStatEntity(
 				scrap_id=r[0],
 				source=r[1],
 				status=r[2],
@@ -147,6 +68,32 @@ class RepositorySourceScrapper(object):
 
 		binds = {
 			"limit": item_limit,
+		}
+
+		return self._sqllite_api.read(stmt, binds, _row_mapper)
+
+	def read_recent_items(self, item_limit: int) -> List[ScrapItemEntity]:
+		def _row_mapper(r) -> ScrapItemEntity:
+			return ScrapItemEntity(
+				source=self._scrap_source.value,
+				scrap_timestamp=Formatter.str_to_ts(TimestampFormat.DATETIME_MS, f"{r[0]} {r[1]}"),
+				name=r[2],
+				local_path=r[3],
+				impressions=r[4]
+			)
+
+		stmt = f"""
+			select ts_date, ts_time, name, local_path, impressions
+			from {_Tables.SCRAP_ITEMS.value}
+			inner join {_Tables.SCRAP_STAT.value}
+				on {_Tables.SCRAP_STAT.value}.scrap_stat_id={_Tables.SCRAP_ITEMS.value}.scrap_stat_id
+			where source=:source
+			order by ts_date desc, ts_time desc
+			limit :limit"""
+
+		binds = {
+			"source": self._scrap_source.value,
+			"limit": item_limit
 		}
 
 		return self._sqllite_api.read(stmt, binds, _row_mapper)
---
+++ mrepositoryentities.py
@@ -0,0 +1,54 @@
+from dataclasses import dataclass
+from datetime import datetime
+from mformatters import Formatter, TimestampFormat
+
+
+@dataclass
+class ScrapStatEntity:
+	scrap_id: int
+	source: str
+	status: str
+	scrap_start: datetime | None
+	scrap_end: datetime | None
+	count_success: int
+	count_fail: int
+	exc_type: str | None
+	exc_value: str | None
+	exc_traceback: str | None
+
+	@property
+	def ts_start(self) -> str:
+		return Formatter.NOT_AVAILABLE_STR if self.scrap_start is None else Formatter.ts_to_str(TimestampFormat.DATETIME, self.scrap_start)
+
+	@property
+	def ts_end(self) -> str:
+		return Formatter.NOT_AVAILABLE_STR if self.scrap_end is None else Formatter.ts_to_str(TimestampFormat.DATETIME, self.scrap_end)
+
+	@property
+	def age(self) -> str:
+		return Formatter.NOT_AVAILABLE_STR if self.scrap_start is None else Formatter.ts_diff_to_str(self.scrap_start, datetime.now(),False)
+
+	@property
+	def time_taken(self) -> str:
+		return Formatter.NOT_AVAILABLE_STR if None in (self.scrap_start, self.scrap_end) else Formatter.ts_diff_to_str(self.scrap_start, self.scrap_end, False)
+
+	@property
+	def success_percentage(self) -> str:
+		return Formatter.percentage_str_safe(self.count_success, self.count_success + self.count_fail, Formatter.NOT_AVAILABLE_STR)
+
+
+@dataclass
+class ScrapItemEntity:
+	source: str
+	scrap_timestamp: datetime
+	name: str
+	local_path: str
+	impressions: int
+
+	@property
+	def age(self) -> str:
+		return Formatter.ts_diff_to_str(self.scrap_timestamp, datetime.now(), False)
+
+	@property
+	def scrap_timestamp_str(self):
+		return Formatter.ts_to_str(TimestampFormat.DATETIME)
