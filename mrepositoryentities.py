f050870c24f4af1a4f7d18bcccaf973fb444e1d1
Coufal, Jan <jan.coufal@oracle.com>
2024-01-10 22:54:49 +0100

    mrepositoryentities.py

--- mrepositoryentities.py
+++ mrepositoryentities.py
@@ -4,6 +4,75 @@ from mformatters import Formatter, TimestampFormat
 
 
 @dataclass
+class MScrapTaskE:
+	pk_id: int | None
+	scrapper: str
+	ts_start: str
+	ts_end: str | None
+	status: str
+	item_count_success: int
+	item_count_fail: int
+	exception_type: str | None
+	exception_value: str | None
+
+	@property
+	def start_as_timestamp(self) -> datetime | None:
+		return Formatter.str_to_ts_safe(TimestampFormat.DATETIME_MS, self.ts_start, None)
+
+	@property
+	def end_as_timestamp(self) -> datetime | None:
+		return Formatter.str_to_ts_safe(TimestampFormat.DATETIME_MS, self.ts_end, None)
+
+	@property
+	def age(self) -> str:
+		s = self.start_as_timestamp
+		return Formatter.NOT_AVAILABLE_STR if s is None else Formatter.ts_diff_to_str(s, datetime.now(), False)
+
+	@property
+	def time_taken(self) -> str:
+		s, e = self.start_as_timestamp, self.end_as_timestamp
+		return Formatter.NOT_AVAILABLE_STR if None in (s, e) else Formatter.ts_diff_to_str(s, e, False)
+
+	@property
+	def success_percentage(self) -> str:
+		s, f = self.item_count_success, self.item_count_fail
+		return Formatter.percentage_str_safe(s, s+f, Formatter.NOT_AVAILABLE_STR)
+
+
+@dataclass
+class MScrapTaskItemE:
+	pk_id: int | None
+	task_id: int
+	ts_start: str
+	ts_end: str | None
+	status: str
+	item_name: str
+	local_path: str | None
+	exception_type: str | None
+	exception_value: str | None
+
+	@property
+	def start_as_timestamp(self) -> datetime | None:
+		return Formatter.str_to_ts_safe(TimestampFormat.DATETIME_MS, self.ts_start, None)
+
+	@property
+	def end_as_timestamp(self) -> datetime | None:
+		return Formatter.str_to_ts_safe(TimestampFormat.DATETIME_MS, self.ts_end, None)
+
+	@property
+	def age(self) -> str:
+		s = self.start_as_timestamp
+		return Formatter.NOT_AVAILABLE_STR if s is None else Formatter.ts_diff_to_str(s, datetime.now(), False)
+
+	@property
+	def time_taken(self) -> str:
+		s, e = self.start_as_timestamp, self.end_as_timestamp
+		return Formatter.NOT_AVAILABLE_STR if None in (s, e) else Formatter.ts_diff_to_str(s, e, False)
+
+
+# TODO: remove
+"""
+@dataclass
 class ScrapStatEntity:
 	scrap_id: int
 	source: str
@@ -16,13 +85,33 @@ class ScrapStatEntity:
 	exc_value: str | None
 	exc_traceback: str | None
 
+	@staticmethod
+	def _format_ts(dt: datetime, formatter: TimestampFormat) -> str:
+		return Formatter.ts_to_str(formatter, dt) if dt is not None else Formatter.NOT_AVAILABLE_STR
+
 	@property
 	def ts_start(self) -> str:
-		return Formatter.NOT_AVAILABLE_STR if self.scrap_start is None else Formatter.ts_to_str(TimestampFormat.DATETIME, self.scrap_start)
+		return ScrapStatEntity._format_ts(self.scrap_start, TimestampFormat.DATETIME)
+
+	@property
+	def ts_start_week(self) -> str:
+		return ScrapStatEntity._format_ts(self.scrap_start, TimestampFormat.WEEK)
+
+	@property
+	def ts_start_date_and_time(self) -> tuple[str, str]:
+		d = ScrapStatEntity._format_ts(self.scrap_start, TimestampFormat.DATE)
+		t = ScrapStatEntity._format_ts(self.scrap_start, TimestampFormat.TIME)
+		return d, t
 
 	@property
 	def ts_end(self) -> str:
-		return Formatter.NOT_AVAILABLE_STR if self.scrap_end is None else Formatter.ts_to_str(TimestampFormat.DATETIME, self.scrap_end)
+		return ScrapStatEntity._format_ts(self.scrap_end, TimestampFormat.DATETIME)
+
+	@property
+	def ts_end_date_and_time(self) -> tuple[str, str]:
+		d = ScrapStatEntity._format_ts(self.scrap_end, TimestampFormat.DATE)
+		t = ScrapStatEntity._format_ts(self.scrap_end, TimestampFormat.TIME)
+		return d, t
 
 	@property
 	def age(self) -> str:
@@ -52,3 +141,4 @@ class ScrapItemEntity:
 	@property
 	def scrap_timestamp_str(self):
 		return Formatter.ts_to_str(TimestampFormat.DATETIME)
+"""
